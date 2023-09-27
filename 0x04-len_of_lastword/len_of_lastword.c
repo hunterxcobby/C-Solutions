009e9e204012a3040fb8c55fd2016c56e6500c15
hunterxcobby <solomonsefah13@gmail.com>
2023-09-27 23:16:47 +0000

    fixed betty issues

--- 0x04-len_of_lastword/len_of_lastword.c
+++ 0x04-len_of_lastword/len_of_lastword.c
@@ -1,18 +1,19 @@
-int lengthOfLastWord(char * s){
+int lengthOfLastWord(char *s)
+{
 
-    int len = strlen(s);
-    int length = 0;
+	int len = strlen(s);
+	int length = 0;
 
-    /* Handling trailing whitespaces */
-    while (len >  0 && s[len - 1] == ' ')
-        len--;
+	/* Handling trailing whitespaces */
+	while (len >  0 && s[len - 1] == ' ')
+		len--;
 
-    /* Find the last word */
-    while (len > 0 && s[len - 1] != ' ')
-    {
-        len--;
-        length++;
-    }
+	/* Find the last word */
+	while (len > 0 && s[len - 1] != ' ')
+	{
+		len--;
+		length++;
+	}
 
-    return length;
+	return (length);
 }
