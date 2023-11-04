827741799bb18cd40d1c26e35d179387b67e3a82
hunterxcobby <solomonsefah13@gmail.com>
2023-11-04 20:00:14 +0000

    a function that compares for values and returns the max

---
+++ 1-HackerRank/0x04-functions_callenge/max_funtion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d);
+
+int main() 
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int result = a; // Initialize result with a as default
+
+    if (b > result)
+        result = b;
+    
+    if (c > result)
+        result = c;
+    
+    if (d > result)
+        result = d;
+
+    return result;
+}
