e570aa841f3a67c8897348bd818f86a832cded1d
hunterxcobby <solomonsefah13@gmail.com>
2023-11-05 14:45:16 +0000

    a program that updates the values of two variables using pointers

---
+++ 1-HackerRank/0x05-Pointers_challenge/solution.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void update(int *a, int *b) 
+{
+    int temp_a = *a;  /* Store the original value of a*/
+    
+    *a = *a + *b;  /* Update a*/
+    
+    if (temp_a > *b) {
+        *b = temp_a - *b;  /* Update b with the absolute difference */
+    } 
+    else 
+    {
+        *b = *b - temp_a;  /* Update b with the absolute difference*/
+    }
+}
+
+
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
